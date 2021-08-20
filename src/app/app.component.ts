7a1ce438ad574396597c8594a9418ad1fae4e4e1
Yasser Nabil <ynabil14@gmail.com>
2021-08-20 21:00:58 +0100

    update appComponent
    updated the github api link to be more flexible and added some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,18 @@ export class AppComponent {
   title = 'MostStarredRepositories';
 
   gitRepo = [];
-  
+
+  //get current time and remove 30 days 
+  time = new Date(Date.now()-2628000000).toLocaleDateString().split("/");
+
+  //change to the format of the github api
+  date = this.time[2] + "-" + this.time[1] + "-" + this.time[0]; 
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     //subscribe data from the github api to get the repositories sorted in terms of stars
-    this.http.get("https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc")
+    this.http.get("https://api.github.com/search/repositories?q=created:%3E"+this.date+"&sort=stars&order=desc&page="+this.page)
     .subscribe( data => {
       //assign the JSON data of the repository to gitRepo
       this.gitRepo = data.items;
