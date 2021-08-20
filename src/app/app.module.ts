dc040cfd6c5f705c73e25231e5e23a7886bc121b
Yasser Nabil <ynabil14@gmail.com>
2021-08-20 20:17:35 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RepositoryComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
